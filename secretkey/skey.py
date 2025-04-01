0a924f0c59ddc809638f5fe9ebcc9c36a0df838f
Gilles Buffat <gbuffat@gmail.com>
2025-04-01 23:42:36 +0200

    Delete secretkey directory

--- secretkey/skey.py
+++
@@ -1,6 +0,0 @@
-import secrets
-import string
-
-alphabet = string.ascii_letters + string.digits + string.punctuation
-secret_key = ''.join(secrets.choice(alphabet) for i in range(50))  # Adjust length as needed
-print(secret_key)
